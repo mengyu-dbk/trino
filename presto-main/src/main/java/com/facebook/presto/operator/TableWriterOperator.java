mengyu-dbk/trino

commit 906ec7155b8757ae612b6c5684aa9b619e2c45b0

Remove field from Input

presto-main/src/main/java/com/facebook/presto/sql/planner/LocalExecutionPlanner.java
@@ -118,10 +118,7 @@ import static com.facebook.presto.operator.TableCommitOperator.TableCommitter;
 import static com.facebook.presto.operator.TableWriterOperator.TableWriterOperatorFactory;
 import static com.facebook.presto.sql.planner.plan.JoinNode.EquiJoinClause.leftGetter;
 import static com.facebook.presto.sql.planner.plan.JoinNode.EquiJoinClause.rightGetter;
-import static com.facebook.presto.sql.tree.Input.fieldGetter;
 import static com.google.common.base.Preconditions.checkNotNull;
-import static com.google.common.base.Predicates.equalTo;
-import static com.google.common.base.Predicates.not;
 
 public class LocalExecutionPlanner
 {
@@ -266,7 +263,7 @@ public class LocalExecutionPlanner
             ImmutableMultimap.Builder<Symbol, Input> outputMappings = ImmutableMultimap.builder();
             int channel = 0;
             for (Symbol symbol : node.getOutputSymbols()) {
-                outputMappings.put(symbol, new Input(channel, 0)); // one symbol per channel
+                outputMappings.put(symbol, new Input(channel)); // one symbol per channel
                 channel++;
             }
 
@@ -347,7 +344,7 @@ public class LocalExecutionPlanner
             // window functions go in remaining channels starting after the last channel from the source operator, one per channel
             int channel = source.getTupleInfos().size();
             for (Symbol symbol : windowFunctionOutputSymbols) {
-                outputMappings.put(symbol, new Input(channel, 0));
+                outputMappings.put(symbol, new Input(channel));
                 channel++;
             }
 
@@ -518,7 +515,7 @@ public class LocalExecutionPlanner
                 for (Symbol symbol : tableScanNode.getOutputSymbols()) {
                     columns.add(tableScanNode.getAssignments().get(symbol));
 
-                    Input input = new Input(channel, 0);
+                    Input input = new Input(channel);
                     sourceLayout.put(symbol, input);
 
                     Type type = checkNotNull(context.getTypes().get(symbol), "No type for symbol %s", symbol);
@@ -538,7 +535,7 @@ public class LocalExecutionPlanner
             ImmutableMultimap.Builder<Symbol, Input> outputMappingsBuilder = ImmutableMultimap.builder();
             for (int i = 0; i < outputSymbols.size(); i++) {
                 Symbol symbol = outputSymbols.get(i);
-                outputMappingsBuilder.put(symbol, new Input(i, 0)); // one field per channel
+                outputMappingsBuilder.put(symbol, new Input(i)); // one field per channel
             }
             Multimap<Symbol, Input> outputMappings = outputMappingsBuilder.build();
 
@@ -656,7 +653,7 @@ public class LocalExecutionPlanner
             for (Symbol symbol : node.getOutputSymbols()) {
                 columns.add(node.getAssignments().get(symbol));
 
-                outputMappings.put(symbol, new Input(channel, 0)); // one column per channel
+                outputMappings.put(symbol, new Input(channel)); // one column per channel
                 channel++;
             }
 
@@ -723,7 +720,7 @@ public class LocalExecutionPlanner
             int offset = probeSource.getTupleInfos().size();
             for (Map.Entry<Symbol, Input> entry : buildSource.getLayout().entries()) {
                 Input input = entry.getValue();
-                outputMappings.put(entry.getKey(), new Input(offset + input.getChannel(), input.getField()));
+                outputMappings.put(entry.getKey(), new Input(offset + input.getChannel()));
             }
 
             OperatorFactory operator = createJoinOperator(node.getType(), hashSupplier, probeSource.getTupleInfos(), probeChannels, context);
@@ -775,7 +772,7 @@ public class LocalExecutionPlanner
             // Source channels are always laid out first, followed by the boolean output symbol
             ImmutableMultimap<Symbol, Input> outputMappings = ImmutableMultimap.<Symbol, Input>builder()
                     .putAll(probeSource.getLayout())
-                    .put(node.getSemiJoinOutput(), new Input(probeSource.getLayout().size(), 0))
+                    .put(node.getSemiJoinOutput(), new Input(probeSource.getLayout().size()))
                     .build();
 
             HashSemiJoinOperatorFactory operator = new HashSemiJoinOperatorFactory(context.getNextOperatorId(), setProvider, probeSource.getTupleInfos(), probeChannel);
@@ -787,20 +784,14 @@ public class LocalExecutionPlanner
         {
             PhysicalOperation source = node.getSource().accept(this, context);
 
+            // are the symbols of the source in the same order as the sink expects?
             boolean projectionMatchesOutput = IterableTransformer.on(source.getLayout().entries())
                     .orderBy(inputOrdering().onResultOf(MoreFunctions.<Symbol, Input>valueGetter()))
                     .transform(MoreFunctions.<Symbol, Input>keyGetter())
                     .list()
                     .equals(node.getOutputSymbols());
 
-            // if any symbols are mapped to a non-zero field, re-map to one field per channel
-            // TODO: this is currently what the exchange operator expects -- figure out how to remove this assumption
-            // to avoid unnecessary projections
-            boolean hasMultiFieldChannels = IterableTransformer.on(source.getLayout().values())
-                    .transform(fieldGetter())
-                    .any(not(equalTo(0)));
-
-            if (hasMultiFieldChannels || !projectionMatchesOutput) {
+            if (!projectionMatchesOutput) {
                 IdentityProjectionInfo mappings = computeIdentityMapping(node.getOutputSymbols(), source.getLayout(), context.getTypes());
                 OperatorFactory operatorFactory = new FilterAndProjectOperatorFactory(context.getNextOperatorId(), FilterFunctions.TRUE_FUNCTION, mappings.getProjections());
                 // NOTE: the generated output layout may not be completely accurate if the same field was projected as multiple inputs.
@@ -822,8 +813,8 @@ public class LocalExecutionPlanner
             OperatorFactory operatorFactory = new TableWriterOperatorFactory(context.getNextOperatorId(), recordSink);
 
             Multimap<Symbol, Input> layout = ImmutableMultimap.<Symbol, Input>builder()
-                    .put(node.getOutputSymbols().get(0), new Input(0, 0))
-                    .put(node.getOutputSymbols().get(1), new Input(1, 0))
+                    .put(node.getOutputSymbols().get(0), new Input(0))
+                    .put(node.getOutputSymbols().get(1), new Input(1))
                     .build();
 
             return new PhysicalOperation(operatorFactory, layout, source);
@@ -834,12 +825,7 @@ public class LocalExecutionPlanner
             LocalExecutionPlanContext subContext = context.createSubContext();
             PhysicalOperation source = node.getSource().accept(this, subContext);
 
-            // introduce a project to unpack everything into separate channels
-            source = unpack(source, node.getColumns(), subContext);
-
-            List<TupleInfo> tupleInfos = getSourceOperatorTupleInfos(node, context.getTypes());
-
-            InMemoryExchange exchange = new InMemoryExchange(tupleInfos);
+            InMemoryExchange exchange = new InMemoryExchange(getSourceOperatorTupleInfos(node, context.getTypes()));
 
             // create exchange sink
             List<OperatorFactory> factories = ImmutableList.<OperatorFactory>builder()
@@ -859,19 +845,12 @@ public class LocalExecutionPlanner
             List<Symbol> layout = node.getOutputSymbols();
             ImmutableMultimap.Builder<Symbol, Input> outputMappings = ImmutableMultimap.builder();
             for (int i = 0; i < layout.size(); i++) {
-                outputMappings.put(layout.get(i), new Input(i, 0));
+                outputMappings.put(layout.get(i), new Input(i));
             }
 
             // add exchange source as first operator in the current context
             OperatorFactory factory = new InMemoryExchangeSourceOperatorFactory(context.getNextOperatorId(), exchange);
             return new PhysicalOperation(factory, outputMappings.build());
-        }
-
-        private PhysicalOperation unpack(PhysicalOperation source, List<Symbol> columns, LocalExecutionPlanContext context)
-        {
-            IdentityProjectionInfo mappings = computeIdentityMapping(columns, source.getLayout(), context.getTypes());
-            OperatorFactory operatorFactory = new FilterAndProjectOperatorFactory(context.getNextOperatorId(), FilterFunctions.TRUE_FUNCTION, mappings.getProjections());
-            return new PhysicalOperation(operatorFactory, mappings.getOutputLayout(), source);
         }
 
         @Override
@@ -880,7 +859,7 @@ public class LocalExecutionPlanner
             PhysicalOperation source = node.getSource().accept(this, context);
 
             OperatorFactory operatorFactory = new TableCommitOperatorFactory(context.getNextOperatorId(), createTableCommitter(node, metadata));
-            Multimap<Symbol, Input> layout = ImmutableMultimap.of(getFirst(node.getOutputSymbols()), new Input(0, 0));
+            Multimap<Symbol, Input> layout = ImmutableMultimap.of(getFirst(node.getOutputSymbols()), new Input(0));
 
             return new PhysicalOperation(operatorFactory, layout, source);
         }
@@ -910,7 +889,7 @@ public class LocalExecutionPlanner
                     nodeInfo.getNodeId(),
                     columns.build());
 
-            return new PhysicalOperation(operator, ImmutableMultimap.of(outputSymbol, new Input(0, 0)), source);
+            return new PhysicalOperation(operator, ImmutableMultimap.of(outputSymbol, new Input(0)), source);
         }
 
         @Override
@@ -952,7 +931,7 @@ public class LocalExecutionPlanner
             ImmutableMultimap.Builder<Symbol, Input> outputMappings = ImmutableMultimap.builder();
             int channel = 0;
             for (Symbol symbol : node.getOutputSymbols()) {
-                outputMappings.put(symbol, new Input(channel, 0)); // one symbol per channel
+                outputMappings.put(symbol, new Input(channel)); // one symbol per channel
                 channel++;
             }
 
@@ -1002,7 +981,7 @@ public class LocalExecutionPlanner
                 Symbol symbol = entry.getKey();
 
                 functionDefinitions.add(buildFunctionDefinition(source, node.getFunctions().get(symbol), entry.getValue()));
-                outputMappings.put(symbol, new Input(outputChannel, 0)); // one aggregation per channel
+                outputMappings.put(symbol, new Input(outputChannel)); // one aggregation per channel
                 outputChannel++;
             }
 
@@ -1027,13 +1006,13 @@ public class LocalExecutionPlanner
             // add group-by key fields each in a separate channel
             int channel = 0;
             for (Symbol symbol : groupBySymbols) {
-                outputMappings.put(symbol, new Input(channel, 0));
+                outputMappings.put(symbol, new Input(channel));
                 channel++;
             }
 
             // aggregations go in following channels
             for (Symbol symbol : aggregationOutputSymbols) {
-                outputMappings.put(symbol, new Input(channel, 0));
+                outputMappings.put(symbol, new Input(channel));
                 channel++;
             }
 
@@ -1080,7 +1059,7 @@ public class LocalExecutionPlanner
         for (Symbol symbol : symbols) {
             ProjectionFunction function = ProjectionFunctions.singleColumn(types.get(symbol).getRawType(), getFirst(inputLayout.get(symbol)));
             projections.add(function);
-            outputMappings.put(symbol, new Input(channel, 0)); // one field per channel
+            outputMappings.put(symbol, new Input(channel)); // one field per channel
             channel++;
         }
 
@@ -1192,7 +1171,6 @@ public class LocalExecutionPlanner
             {
                 return ComparisonChain.start()
                         .compare(o1.getChannel(), o2.getChannel())
-                        .compare(o1.getField(), o2.getField())
                         .result();
             }
         });

presto-main/src/test/java/com/facebook/presto/benchmark/CountAggregationBenchmark.java
@@ -44,7 +44,7 @@ public class CountAggregationBenchmark
     {
         BlockIterable blockIterable = getBlockIterable("orders", "orderkey", BlocksFileEncoding.RAW);
         AlignmentOperatorFactory alignmentOperator = new AlignmentOperatorFactory(0, blockIterable);
-        AggregationOperatorFactory aggregationOperator = new AggregationOperatorFactory(1, Step.SINGLE, ImmutableList.of(aggregation(COUNT, new Input(0, 0))));
+        AggregationOperatorFactory aggregationOperator = new AggregationOperatorFactory(1, Step.SINGLE, ImmutableList.of(aggregation(COUNT, new Input(0))));
         return ImmutableList.of(alignmentOperator, aggregationOperator);
     }
 

presto-main/src/test/java/com/facebook/presto/benchmark/DoubleSumAggregationBenchmark.java
@@ -44,7 +44,7 @@ public class DoubleSumAggregationBenchmark
     {
         BlockIterable blockIterable = getBlockIterable("orders", "totalprice", BlocksFileEncoding.RAW);
         AlignmentOperatorFactory alignmentOperator = new AlignmentOperatorFactory(0, blockIterable);
-        AggregationOperatorFactory aggregationOperator = new AggregationOperatorFactory(1, Step.SINGLE, ImmutableList.of(aggregation(DOUBLE_SUM, new Input(0, 0))));
+        AggregationOperatorFactory aggregationOperator = new AggregationOperatorFactory(1, Step.SINGLE, ImmutableList.of(aggregation(DOUBLE_SUM, new Input(0))));
         return ImmutableList.of(alignmentOperator, aggregationOperator);
     }
 

presto-main/src/test/java/com/facebook/presto/benchmark/HandTpchQuery1.java
@@ -107,13 +107,13 @@ public class HandTpchQuery1
                 Ints.asList(0, 1),
                 Step.SINGLE,
                 ImmutableList.of(
-                        aggregation(DOUBLE_SUM, new Input(2, 0)),
-                        aggregation(DOUBLE_SUM, new Input(3, 0)),
-                        aggregation(DOUBLE_SUM, new Input(4, 0)),
-                        aggregation(DOUBLE_AVERAGE, new Input(2, 0)),
-                        aggregation(DOUBLE_AVERAGE, new Input(5, 0)),
-                        aggregation(DOUBLE_AVERAGE, new Input(6, 0)),
-                        aggregation(COUNT, new Input(2, 0))
+                        aggregation(DOUBLE_SUM, new Input(2)),
+                        aggregation(DOUBLE_SUM, new Input(3)),
+                        aggregation(DOUBLE_SUM, new Input(4)),
+                        aggregation(DOUBLE_AVERAGE, new Input(2)),
+                        aggregation(DOUBLE_AVERAGE, new Input(5)),
+                        aggregation(DOUBLE_AVERAGE, new Input(6)),
+                        aggregation(COUNT, new Input(2))
                 ),
                 10_000);
 

presto-main/src/test/java/com/facebook/presto/benchmark/HandTpchQuery6.java
@@ -75,7 +75,7 @@ public class HandTpchQuery6
                 2,
                 Step.SINGLE,
                 ImmutableList.of(
-                        aggregation(DOUBLE_SUM, new Input(0, 0))
+                        aggregation(DOUBLE_SUM, new Input(0))
                 ));
 
         return ImmutableList.of(alignmentOperator, tpchQuery6Operator, aggregationOperator);

presto-main/src/test/java/com/facebook/presto/benchmark/HashAggregationBenchmark.java
@@ -51,7 +51,7 @@ public class HashAggregationBenchmark
                 ImmutableList.of(alignmentOperator.getTupleInfos().get(0)),
                 Ints.asList(0),
                 Step.SINGLE,
-                ImmutableList.of(aggregation(DOUBLE_SUM, new Input(1, 0))),
+                ImmutableList.of(aggregation(DOUBLE_SUM, new Input(1))),
                 100_000);
         return ImmutableList.of(alignmentOperator, aggregationOperator);
     }

presto-main/src/test/java/com/facebook/presto/operator/TestAggregationOperator.java
@@ -78,14 +78,14 @@ public class TestAggregationOperator
         OperatorFactory operatorFactory = new AggregationOperatorFactory(
                 0,
                 Step.SINGLE,
-                ImmutableList.of(aggregation(COUNT, new Input(0, 0)),
-                        aggregation(LONG_SUM, new Input(1, 0)),
-                        aggregation(LONG_AVERAGE, new Input(1, 0)),
-                        aggregation(VAR_BINARY_MAX, new Input(2, 0)),
-                        aggregation(COUNT_STRING_COLUMN, new Input(0, 0)),
-                        aggregation(LONG_SUM, new Input(3, 0)),
-                        aggregation(DOUBLE_SUM, new Input(4, 0)),
-                        aggregation(VAR_BINARY_MAX, new Input(5, 0))));
+                ImmutableList.of(aggregation(COUNT, new Input(0)),
+                        aggregation(LONG_SUM, new Input(1)),
+                        aggregation(LONG_AVERAGE, new Input(1)),
+                        aggregation(VAR_BINARY_MAX, new Input(2)),
+                        aggregation(COUNT_STRING_COLUMN, new Input(0)),
+                        aggregation(LONG_SUM, new Input(3)),
+                        aggregation(DOUBLE_SUM, new Input(4)),
+                        aggregation(VAR_BINARY_MAX, new Input(5))));
         Operator operator = operatorFactory.createOperator(driverContext);
 
         MaterializedResult expected = resultBuilder(FIXED_INT_64, FIXED_INT_64, DOUBLE, VARIABLE_BINARY, FIXED_INT_64, FIXED_INT_64, DOUBLE, VARIABLE_BINARY)

presto-main/src/test/java/com/facebook/presto/operator/TestHashAggregationOperator.java
@@ -88,12 +88,12 @@ public class TestHashAggregationOperator
                 ImmutableList.of(SINGLE_VARBINARY),
                 Ints.asList(1),
                 Step.SINGLE,
-                ImmutableList.of(aggregation(COUNT, new Input(0, 0)),
-                        aggregation(LONG_SUM, new Input(3, 0)),
-                        aggregation(LONG_AVERAGE, new Input(3, 0)),
-                        aggregation(VAR_BINARY_MAX, new Input(2, 0)),
-                        aggregation(COUNT_STRING_COLUMN, new Input(0, 0)),
-                        aggregation(COUNT_BOOLEAN_COLUMN, new Input(4, 0))),
+                ImmutableList.of(aggregation(COUNT, new Input(0)),
+                        aggregation(LONG_SUM, new Input(3)),
+                        aggregation(LONG_AVERAGE, new Input(3)),
+                        aggregation(VAR_BINARY_MAX, new Input(2)),
+                        aggregation(COUNT_STRING_COLUMN, new Input(0)),
+                        aggregation(COUNT_BOOLEAN_COLUMN, new Input(4))),
                 100_000);
 
         Operator operator = operatorFactory.createOperator(driverContext);
@@ -133,10 +133,10 @@ public class TestHashAggregationOperator
                 ImmutableList.of(SINGLE_VARBINARY),
                 Ints.asList(1),
                 Step.SINGLE,
-                ImmutableList.of(aggregation(COUNT, new Input(0, 0)),
-                        aggregation(LONG_SUM, new Input(3, 0)),
-                        aggregation(LONG_AVERAGE, new Input(3, 0)),
-                        aggregation(VAR_BINARY_MAX, new Input(2, 0))),
+                ImmutableList.of(aggregation(COUNT, new Input(0)),
+                        aggregation(LONG_SUM, new Input(3)),
+                        aggregation(LONG_AVERAGE, new Input(3)),
+                        aggregation(VAR_BINARY_MAX, new Input(2))),
                 100_000);
 
         Operator operator = operatorFactory.createOperator(driverContext);
@@ -159,8 +159,8 @@ public class TestHashAggregationOperator
                 ImmutableList.of(SINGLE_LONG),
                 Ints.asList(1),
                 Step.SINGLE,
-                ImmutableList.of(aggregation(COUNT, new Input(0, 0)),
-                        aggregation(LONG_AVERAGE, new Input(1, 0))),
+                ImmutableList.of(aggregation(COUNT, new Input(0)),
+                        aggregation(LONG_AVERAGE, new Input(1))),
                 100_000);
 
         Operator operator = operatorFactory.createOperator(driverContext);

commit 315ad5f1779af95f07ad21a1c95cdd215cb731ff

Fix bug where fields may be written in the wrong order
This bug in CREATE TABLE AS SELECT was introduced by 906ec7155b8757ae612b6c5684aa9b619e2c45b0
with the removal of the call to unpack(...). This of this method was to turn multi-field
channels into one channel per field, but also reordered channels to match what the operator
expected.

presto-main/src/main/java/com/facebook/presto/operator/TableWriterOperator.java
@@ -39,13 +39,15 @@ public class TableWriterOperator
     {
         private final int operatorId;
         private final RecordSink recordSink;
+        private final List<Integer> inputChannels;
         private final List<Type> outputTypes;
         private final Optional<Integer> sampleWeightChannel;
         private boolean closed;
 
-        public TableWriterOperatorFactory(int operatorId, RecordSink recordSink, List<Type> outputTypes, Optional<Integer> sampleWeightChannel)
+        public TableWriterOperatorFactory(int operatorId, RecordSink recordSink, List<Type> outputTypes, List<Integer> inputChannels, Optional<Integer> sampleWeightChannel)
         {
             this.operatorId = operatorId;
+            this.inputChannels = checkNotNull(inputChannels, "inputChannels is null");
             this.recordSink = checkNotNull(recordSink, "recordSink is null");
             this.outputTypes = checkNotNull(outputTypes, "outputTypes is null");
             this.sampleWeightChannel = checkNotNull(sampleWeightChannel, "sampleWeightChannel is null");
@@ -62,7 +64,7 @@ public class TableWriterOperator
         {
             checkState(!closed, "Factory is already closed");
             OperatorContext context = driverContext.addOperatorContext(operatorId, TableWriterOperator.class.getSimpleName());
-            return new TableWriterOperator(context, recordSink, outputTypes, sampleWeightChannel);
+            return new TableWriterOperator(context, recordSink, outputTypes, inputChannels, sampleWeightChannel);
         }
 
         @Override
@@ -81,26 +83,18 @@ public class TableWriterOperator
     private final RecordSink recordSink;
     private final Optional<Integer> sampleWeightChannel;
     private final List<Type> outputTypes;
-    // Maps output channels to the corresponding input channel (identity mapping, unless sample weight channel is present)
-    private final int[] inputChannels;
+    private final List<Integer> inputChannels;
 
     private State state = State.RUNNING;
     private long rowCount;
 
-    public TableWriterOperator(OperatorContext operatorContext, RecordSink recordSink, List<Type> outputTypes, Optional<Integer> sampleWeightChannel)
+    public TableWriterOperator(OperatorContext operatorContext, RecordSink recordSink, List<Type> outputTypes, List<Integer> inputChannels, Optional<Integer> sampleWeightChannel)
     {
         this.operatorContext = checkNotNull(operatorContext, "operatorContext is null");
         this.recordSink = checkNotNull(recordSink, "recordSink is null");
         this.outputTypes = checkNotNull(outputTypes, "outputTypes is null");
         this.sampleWeightChannel = checkNotNull(sampleWeightChannel, "sampleWeightChannel is null");
-        this.inputChannels = new int[outputTypes.size()];
-        for (int inputChannel = 0, outputChannel = 0; outputChannel < inputChannels.length; inputChannel++, outputChannel++) {
-            if (sampleWeightChannel.isPresent() && inputChannel == sampleWeightChannel.get()) {
-                // Skip the sample weight channel in the input, since it's passed to the RecordSink out of band
-                inputChannel++;
-            }
-            inputChannels[outputChannel] = inputChannel;
-        }
+        this.inputChannels = checkNotNull(inputChannels, "inputChannels is null");
     }
 
     @Override
@@ -154,11 +148,11 @@ public class TableWriterOperator
             sampleWeightCursor = page.getBlock(sampleWeightChannel.get()).cursor();
         }
         else {
-            cursors = new BlockCursor[page.getChannelCount()];
+            cursors = new BlockCursor[outputTypes.size()];
         }
 
         for (int outputChannel = 0; outputChannel < cursors.length; outputChannel++) {
-            cursors[outputChannel] = page.getBlock(inputChannels[outputChannel]).cursor();
+            cursors[outputChannel] = page.getBlock(inputChannels.get(outputChannel)).cursor();
         }
 
         int rows = 0;

presto-main/src/main/java/com/facebook/presto/sql/planner/LocalExecutionPlanner.java
@@ -98,7 +98,6 @@ import com.google.common.base.Functions;
 import com.google.common.base.Optional;
 import com.google.common.base.Supplier;
 import com.google.common.collect.ComparisonChain;
-import com.google.common.collect.FluentIterable;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableMap.Builder;
@@ -931,17 +930,17 @@ public class LocalExecutionPlanner
 
             // create the table writer
             RecordSink recordSink = recordSinkManager.getRecordSink(node.getTarget());
-            List<TupleInfo.Type> outputTypes = new ArrayList<>(FluentIterable.from(exchange.getTupleInfos()).transform(new Function<TupleInfo, TupleInfo.Type>() {
-                @Override
-                public TupleInfo.Type apply(TupleInfo input)
-                {
-                    return input.getType();
-                }
-            }).toList());
-            if (sampleWeightChannel.isPresent()) {
-                outputTypes.remove((int) sampleWeightChannel.get());
-            }
-            OperatorFactory operatorFactory = new TableWriterOperatorFactory(context.getNextOperatorId(), recordSink, outputTypes, sampleWeightChannel);
+
+            List<TupleInfo.Type> types = IterableTransformer.on(node.getColumns())
+                    .transform(Functions.forMap(context.getTypes()))
+                    .transform(Type.toRaw())
+                    .list();
+
+            List<Integer> inputChannels = IterableTransformer.on(node.getColumns())
+                    .transform(exchange.channelGetter())
+                    .list();
+
+            OperatorFactory operatorFactory = new TableWriterOperatorFactory(context.getNextOperatorId(), recordSink, types, inputChannels, sampleWeightChannel);
 
             Map<Symbol, Input> layout = ImmutableMap.<Symbol, Input>builder()
                     .put(node.getOutputSymbols().get(0), new Input(0))

presto-server/src/test/java/com/facebook/presto/server/TestDistributedQueries.java
@@ -134,7 +134,7 @@ public class TestDistributedQueries
     {
         assertCreateTable(
                 "test_simple",
-                "SELECT orderkey, totalprice, orderdate FROM orders",
+                "SELECT orderdate, orderkey, totalprice FROM orders",
                 "SELECT count(*) FROM orders");
     }
 