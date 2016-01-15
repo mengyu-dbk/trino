mengyu-dbk/trino

commit ea9e458e79221754a73741c44b31aa387b3c928d

Generalize parametric types
Parametric types are extended to support arguments of kinds other than types.
This is in preparation to support types that take numbers as arguments (e.g,
VARCHAR, DECIMAL)

The "literalParameters" abstraction is now deprecated, but kept around to
support ROW types.

presto-client/src/main/java/com/facebook/presto/client/ClientTypeSignature.java
@@ -13,6 +13,7 @@
  */
 package com.facebook.presto.client;
 
+import com.facebook.presto.spi.type.StandardTypes;
 import com.facebook.presto.spi.type.TypeSignature;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -34,21 +35,21 @@ public class ClientTypeSignature
 {
     private static final Pattern PATTERN = Pattern.compile(".*[<>,].*");
     private final String rawType;
-    private final List<ClientTypeSignature> typeArguments;
+    private final List<ClientTypeSignatureParameter> typeArguments;
     private final List<Object> literalArguments;
 
     public ClientTypeSignature(TypeSignature typeSignature)
     {
         this(
                 typeSignature.getBase(),
-                Lists.transform(typeSignature.getParameters(), ClientTypeSignature::new),
+                Lists.transform(typeSignature.getParameters(), ClientTypeSignatureParameter::new),
                 typeSignature.getLiteralParameters());
     }
 
     @JsonCreator
     public ClientTypeSignature(
             @JsonProperty("rawType") String rawType,
-            @JsonProperty("typeArguments") List<ClientTypeSignature> typeArguments,
+            @JsonProperty("typeArguments") List<ClientTypeSignatureParameter> typeArguments,
             @JsonProperty("literalArguments") List<Object> literalArguments)
     {
         checkArgument(rawType != null, "rawType is null");
@@ -71,7 +72,7 @@ public class ClientTypeSignature
     }
 
     @JsonProperty
-    public List<ClientTypeSignature> getTypeArguments()
+    public List<ClientTypeSignatureParameter> getTypeArguments()
     {
         return typeArguments;
     }
@@ -85,18 +86,34 @@ public class ClientTypeSignature
     @Override
     public String toString()
     {
+        if (rawType.equals(StandardTypes.ROW)) {
+            return rowToString();
+        }
+        else {
+            return toString("(", ")");
+        }
+    }
+
+    @Deprecated
+    private String rowToString()
+    {
+        return toString("<", ">");
+    }
+
+    private String toString(String leftParameterBracket, String rightParameterBracket)
+    {
         StringBuilder typeName = new StringBuilder(rawType);
         if (!typeArguments.isEmpty()) {
-            typeName.append("<");
+            typeName.append(leftParameterBracket);
             boolean first = true;
-            for (ClientTypeSignature argument : typeArguments) {
+            for (ClientTypeSignatureParameter argument : typeArguments) {
                 if (!first) {
                     typeName.append(",");
                 }
                 first = false;
                 typeName.append(argument.toString());
             }
-            typeName.append(">");
+            typeName.append(rightParameterBracket);
         }
         if (!literalArguments.isEmpty()) {
             typeName.append("(");

presto-client/src/main/java/com/facebook/presto/client/ClientTypeSignatureParameter.java
@@ -1,0 +1,114 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.client;
+
+import com.facebook.presto.spi.type.ParameterKind;
+import com.facebook.presto.spi.type.TypeSignatureParameter;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.annotation.concurrent.Immutable;
+
+import java.util.Objects;
+
+import static java.lang.String.format;
+
+@Immutable
+public class ClientTypeSignatureParameter
+{
+    private final ParameterKind kind;
+    private final Object value;
+
+    public ClientTypeSignatureParameter(TypeSignatureParameter typeParameterSignature)
+    {
+        this.kind = typeParameterSignature.getKind();
+        switch (kind) {
+            case TYPE_SIGNATURE:
+                value = new ClientTypeSignature(typeParameterSignature.getTypeSignature());
+                break;
+            case LONG_LITERAL:
+                value = typeParameterSignature.getLongLiteral();
+                break;
+            default:
+                throw new UnsupportedOperationException(format("Unknown kind [%s]", kind));
+        }
+    }
+
+    @JsonCreator
+    public ClientTypeSignatureParameter(
+            @JsonProperty("kind") ParameterKind kind,
+            @JsonProperty("value") Object value)
+    {
+        this.kind = kind;
+        this.value = value;
+    }
+
+    @JsonProperty
+    public ParameterKind getKind()
+    {
+        return kind;
+    }
+
+    @JsonProperty
+    public Object getValue()
+    {
+        return value;
+    }
+
+    private <A> A getValue(ParameterKind expectedParameterKind, Class<A> target)
+    {
+        if (kind != expectedParameterKind) {
+            throw new IllegalArgumentException(format("ParameterKind is [%s] but expected [%s]", kind, expectedParameterKind));
+        }
+        return target.cast(value);
+    }
+
+    public ClientTypeSignature getTypeSignature()
+    {
+        return getValue(ParameterKind.TYPE_SIGNATURE, ClientTypeSignature.class);
+    }
+
+    public Long getLongLiteral()
+    {
+        return getValue(ParameterKind.LONG_LITERAL, Long.class);
+    }
+
+    @Override
+    public String toString()
+    {
+        return value.toString();
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        ClientTypeSignatureParameter other = (ClientTypeSignatureParameter) o;
+
+        return Objects.equals(this.kind, other.kind) &&
+                Objects.equals(this.value, other.value);
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return Objects.hash(kind, value);
+    }
+}

presto-client/src/main/java/com/facebook/presto/client/QueryResults.java
@@ -224,13 +224,13 @@ public class QueryResults
         if (signature.getBase().equals(ARRAY)) {
             List<Object> fixedValue = new ArrayList<>();
             for (Object object : List.class.cast(value)) {
-                fixedValue.add(fixValue(signature.getParameters().get(0).toString(), object));
+                fixedValue.add(fixValue(signature.getTypeParametersAsTypeSignatures().get(0).toString(), object));
             }
             return fixedValue;
         }
         if (signature.getBase().equals(MAP)) {
-            String keyType = signature.getParameters().get(0).toString();
-            String valueType = signature.getParameters().get(1).toString();
+            String keyType = signature.getTypeParametersAsTypeSignatures().get(0).toString();
+            String valueType = signature.getTypeParametersAsTypeSignatures().get(1).toString();
             Map<Object, Object> fixedValue = new HashMap<>();
             for (Map.Entry<?, ?> entry : (Set<Map.Entry<?, ?>>) Map.class.cast(value).entrySet()) {
                 fixedValue.put(fixValue(keyType, entry.getKey()), fixValue(valueType, entry.getValue()));
@@ -243,7 +243,7 @@ public class QueryResults
             checkArgument(listValue.size() == signature.getLiteralParameters().size(), "Mismatched data values and row type");
             for (int i = 0; i < listValue.size(); i++) {
                 String key = (String) signature.getLiteralParameters().get(i);
-                fixedValue.put(key, fixValue(signature.getParameters().get(i).toString(), listValue.get(i)));
+                fixedValue.put(key, fixValue(signature.getTypeParametersAsTypeSignatures().get(i).toString(), listValue.get(i)));
             }
             return fixedValue;
         }

presto-jdbc/src/main/java/com/facebook/presto/jdbc/ColumnInfo.java
@@ -14,6 +14,7 @@
 package com.facebook.presto.jdbc;
 
 import com.facebook.presto.spi.type.TypeSignature;
+import com.facebook.presto.spi.type.TypeSignatureParameter;
 import com.google.common.collect.ImmutableList;
 
 import java.sql.Types;
@@ -88,7 +89,7 @@ class ColumnInfo
     {
         builder.setColumnType(getType(type));
         ImmutableList.Builder<Integer> parameterTypes = ImmutableList.builder();
-        for (TypeSignature parameter : type.getParameters()) {
+        for (TypeSignatureParameter parameter : type.getParameters()) {
             parameterTypes.add(getType(parameter));
         }
         builder.setColumnParameterTypes(parameterTypes.build());
@@ -155,6 +156,16 @@ class ColumnInfo
             case "interval day to second":
                 builder.setColumnDisplaySize(TIMESTAMP_MAX);
                 break;
+        }
+    }
+
+    private static int getType(TypeSignatureParameter typeParameter)
+    {
+        switch (typeParameter.getKind()) {
+            case TYPE_SIGNATURE:
+                return getType(typeParameter.getTypeSignature());
+            default:
+                return Types.JAVA_OBJECT;
         }
     }
 

presto-kafka/src/test/java/com/facebook/presto/kafka/TestKafkaPlugin.java
@@ -22,6 +22,7 @@ import com.facebook.presto.spi.NodeState;
 import com.facebook.presto.spi.type.Type;
 import com.facebook.presto.spi.type.TypeManager;
 import com.facebook.presto.spi.type.TypeSignature;
+import com.facebook.presto.spi.type.TypeSignatureParameter;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
@@ -70,6 +71,12 @@ public class TestKafkaPlugin
     {
         @Override
         public Type getType(TypeSignature signature)
+        {
+            return null;
+        }
+
+        @Override
+        public Type getParameterizedType(String baseTypeName, List<TypeSignatureParameter> typeParameters)
         {
             return null;
         }

presto-main/src/main/java/com/facebook/presto/metadata/FunctionRegistry.java
@@ -204,7 +204,6 @@ import static com.facebook.presto.util.ImmutableCollectors.toImmutableSet;
 import static com.facebook.presto.util.Types.checkType;
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkState;
-import static com.google.common.base.Verify.verify;
 import static java.lang.String.format;
 import static java.util.Objects.requireNonNull;
 
@@ -381,10 +380,10 @@ public class FunctionRegistry
         }
         ImmutableList.Builder<TypeSignature> boundArguments = ImmutableList.builder();
         for (int i = 0; i < argumentTypes.size() - 1; i++) {
-            boundArguments.add(bindParameters(argumentTypes.get(i), boundParameters));
+            boundArguments.add(argumentTypes.get(i).bindParameters(boundParameters));
         }
         if (!argumentTypes.isEmpty()) {
-            TypeSignature lastArgument = bindParameters(argumentTypes.get(argumentTypes.size() - 1), boundParameters);
+            TypeSignature lastArgument = argumentTypes.get(argumentTypes.size() - 1).bindParameters(boundParameters);
             if (signature.isVariableArity()) {
                 for (int i = 0; i < types.size() - (argumentTypes.size() - 1); i++) {
                     boundArguments.add(lastArgument);
@@ -394,18 +393,7 @@ public class FunctionRegistry
                 boundArguments.add(lastArgument);
             }
         }
-        return new Signature(signature.getName(), signature.getKind(), bindParameters(signature.getReturnType(), boundParameters), boundArguments.build());
-    }
-
-    private static TypeSignature bindParameters(TypeSignature typeSignature, Map<String, Type> boundParameters)
-    {
-        List<TypeSignature> parameters = typeSignature.getParameters().stream().map(signature -> bindParameters(signature, boundParameters)).collect(toImmutableList());
-        String base = typeSignature.getBase();
-        if (boundParameters.containsKey(base)) {
-            verify(typeSignature.getLiteralParameters().isEmpty() && typeSignature.getParameters().isEmpty(), "Type parameters cannot have parameters");
-            return boundParameters.get(base).getTypeSignature();
-        }
-        return new TypeSignature(base, parameters, typeSignature.getLiteralParameters());
+        return new Signature(signature.getName(), signature.getKind(), signature.getReturnType().bindParameters(boundParameters), boundArguments.build());
     }
 
     public final synchronized void addFunctions(List<? extends SqlFunction> functions)

presto-main/src/main/java/com/facebook/presto/metadata/OperatorType.java
@@ -158,11 +158,11 @@ public enum OperatorType
                     checkArgument(argumentTypes.get(0).getBase().equals(StandardTypes.ARRAY) || argumentTypes.get(0).getBase().equals(StandardTypes.MAP), "First argument must be an ARRAY or MAP");
                     if (argumentTypes.get(0).getBase().equals(StandardTypes.ARRAY)) {
                         checkArgument(argumentTypes.get(1).getBase().equals(StandardTypes.BIGINT), "Second argument must be a BIGINT");
-                        TypeSignature elementType = argumentTypes.get(0).getParameters().get(0);
+                        TypeSignature elementType = argumentTypes.get(0).getTypeParametersAsTypeSignatures().get(0);
                         checkArgument(returnType.equals(elementType), "[] return type does not match ARRAY element type");
                     }
                     else {
-                        TypeSignature valueType = argumentTypes.get(0).getParameters().get(1);
+                        TypeSignature valueType = argumentTypes.get(0).getTypeParametersAsTypeSignatures().get(1);
                         checkArgument(returnType.equals(valueType), "[] return type does not match MAP value type");
                     }
                 }

presto-main/src/main/java/com/facebook/presto/metadata/Signature.java
@@ -292,9 +292,11 @@ public final class Signature
 
     private static boolean matchAndBind(Map<String, Type> boundParameters, Map<String, TypeParameter> typeParameters, TypeSignature parameter, Type type, boolean allowCoercion, TypeManager typeManager)
     {
+        // TODO: add literals to Types and switch to TypeSignatureParameter so boundParameters map includes also literal parameter bindings
+        List<TypeSignature> typeSignatures = parameter.getTypeParametersAsTypeSignatures();
         // If this parameter is already bound, then match (with coercion)
         if (boundParameters.containsKey(parameter.getBase())) {
-            checkArgument(parameter.getParameters().isEmpty(), "Unexpected parameteric type");
+            checkArgument(typeSignatures.isEmpty(), "Unexpected parameteric type");
             if (allowCoercion) {
                 if (canCoerce(type, boundParameters.get(parameter.getBase()))) {
                     return true;
@@ -312,13 +314,13 @@ public final class Signature
         }
 
         // Recurse into component types
-        if (!parameter.getParameters().isEmpty()) {
-            if (type.getTypeParameters().size() != parameter.getParameters().size()) {
+        if (!typeSignatures.isEmpty()) {
+            if (type.getTypeParameters().size() != typeSignatures.size()) {
                 return false;
             }
-            for (int i = 0; i < parameter.getParameters().size(); i++) {
+            for (int i = 0; i < typeSignatures.size(); i++) {
                 Type componentType = type.getTypeParameters().get(i);
-                TypeSignature componentSignature = parameter.getParameters().get(i);
+                TypeSignature componentSignature = typeSignatures.get(i);
                 if (!matchAndBind(boundParameters, typeParameters, componentSignature, componentType, allowCoercion, typeManager)) {
                     return false;
                 }
@@ -336,7 +338,7 @@ public final class Signature
         }
 
         // We've already checked all the components, so just match the base type
-        if (!parameter.getParameters().isEmpty()) {
+        if (!typeSignatures.isEmpty()) {
             return type.getTypeSignature().getBase().equals(parameter.getBase());
         }
 

commit 6345d452a464adbab8e090b86d8a0ddd21d253d9

Fix compatibility issue in ClientTypeSignature
A series of commits, starting with
ea9e458e79221754a73741c44b31aa387b3c928d broke compatibility between the
JDBC driver and Presto servers version < 0.133, this fixes the JDBC
driver to be compatible with all versions of the server

presto-client/src/main/java/com/facebook/presto/client/ClientTypeSignature.java
@@ -14,10 +14,13 @@
 package com.facebook.presto.client;
 
 import com.facebook.presto.spi.type.NamedTypeSignature;
+import com.facebook.presto.spi.type.ParameterKind;
 import com.facebook.presto.spi.type.StandardTypes;
 import com.facebook.presto.spi.type.TypeSignature;
+import com.facebook.presto.spi.type.TypeSignatureParameter;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
 
 import javax.annotation.concurrent.Immutable;
@@ -32,6 +35,8 @@ import java.util.stream.Collectors;
 import static com.google.common.base.Preconditions.checkArgument;
 import static java.lang.String.format;
 import static java.util.Collections.unmodifiableList;
+import static java.util.Objects.requireNonNull;
+import static java.util.stream.Collectors.toList;
 
 @Immutable
 public class ClientTypeSignature
@@ -47,17 +52,68 @@ public class ClientTypeSignature
                 Lists.transform(typeSignature.getParameters(), ClientTypeSignatureParameter::new));
     }
 
+    public ClientTypeSignature(String rawType, List<ClientTypeSignatureParameter> arguments)
+    {
+        this(rawType, ImmutableList.of(), ImmutableList.of(), arguments);
+    }
+
     @JsonCreator
     public ClientTypeSignature(
             @JsonProperty("rawType") String rawType,
+            @JsonProperty("typeArguments") List<ClientTypeSignature> typeArguments,
+            @JsonProperty("literalArguments") List<Object> literalArguments,
             @JsonProperty("arguments") List<ClientTypeSignatureParameter> arguments)
     {
-        checkArgument(rawType != null, "rawType is null");
+        requireNonNull(rawType, "rawType is null");
         this.rawType = rawType;
         checkArgument(!rawType.isEmpty(), "rawType is empty");
         checkArgument(!PATTERN.matcher(rawType).matches(), "Bad characters in rawType type: %s", rawType);
-        checkArgument(arguments != null, "arguments is null");
-        this.arguments = unmodifiableList(new ArrayList<>(arguments));
+        if (arguments != null) {
+            this.arguments = unmodifiableList(new ArrayList<>(arguments));
+        }
+        else {
+            requireNonNull(typeArguments, "typeArguments is null");
+            requireNonNull(literalArguments, "literalArguments is null");
+            ImmutableList.Builder<ClientTypeSignatureParameter> convertedArguments = ImmutableList.builder();
+            // Talking to a legacy server (< 0.133)
+            if (rawType.equals(StandardTypes.ROW)) {
+                checkArgument(typeArguments.size() == literalArguments.size());
+                for (int i = 0; i < typeArguments.size(); i++) {
+                    Object value = literalArguments.get(i);
+                    checkArgument(value instanceof String, "Expected literalArgument %d in %s to be a string", i, literalArguments);
+                    convertedArguments.add(new ClientTypeSignatureParameter(TypeSignatureParameter.of(new NamedTypeSignature((String) value, toTypeSignature(typeArguments.get(i))))));
+                }
+            }
+            else {
+                checkArgument(literalArguments.isEmpty(), "Unexpected literal arguments from legacy server");
+                for (ClientTypeSignature typeArgument : typeArguments) {
+                    convertedArguments.add(new ClientTypeSignatureParameter(ParameterKind.TYPE_SIGNATURE, typeArgument));
+                }
+            }
+            this.arguments = convertedArguments.build();
+        }
+    }
+
+    private static TypeSignature toTypeSignature(ClientTypeSignature signature)
+    {
+        List<TypeSignatureParameter> parameters = signature.getArguments().stream()
+                .map(ClientTypeSignature::legacyClientTypeSignatureParameterToTypeSignatureParameter)
+                .collect(toList());
+        return new TypeSignature(signature.getRawType(), parameters);
+    }
+
+    private static TypeSignatureParameter legacyClientTypeSignatureParameterToTypeSignatureParameter(ClientTypeSignatureParameter parameter)
+    {
+        switch (parameter.getKind()) {
+            case LONG_LITERAL:
+                throw new UnsupportedOperationException("Unexpected long type literal returned by legacy server");
+            case TYPE_SIGNATURE:
+                return TypeSignatureParameter.of(toTypeSignature(parameter.getTypeSignature()));
+            case NAMED_TYPE_SIGNATURE:
+                return TypeSignatureParameter.of(parameter.getNamedTypeSignature());
+            default:
+                throw new UnsupportedOperationException("Unknown parameter kind " + parameter.getKind());
+        }
     }
 
     @JsonProperty

presto-client/src/test/java/com/facebook/presto/client/TestClientTypeSignature.java
@@ -14,6 +14,7 @@
 package com.facebook.presto.client;
 
 import com.facebook.presto.spi.type.NamedTypeSignature;
+import com.facebook.presto.spi.type.StandardTypes;
 import com.facebook.presto.spi.type.TypeSignature;
 import com.facebook.presto.spi.type.TypeSignatureParameter;
 import com.google.common.collect.ImmutableList;
@@ -51,6 +52,14 @@ public class TestClientTypeSignature
                 ImmutableList.of(
                         new ClientTypeSignatureParameter(TypeSignatureParameter.of(new NamedTypeSignature("foo", bigint))),
                         new ClientTypeSignatureParameter(TypeSignatureParameter.of(new NamedTypeSignature("bar", bigint))))));
+    }
+
+    @Test
+    public void testBackwardsCompatible()
+    {
+        ClientTypeSignature signature = new ClientTypeSignature(StandardTypes.ARRAY, ImmutableList.of(new ClientTypeSignatureParameter(TypeSignatureParameter.of(BIGINT.getTypeSignature()))));
+        ClientTypeSignature legacy = CLIENT_TYPE_SIGNATURE_CODEC.fromJson("{\"rawType\":\"array\",\"literalArguments\":[],\"typeArguments\":[{\"rawType\":\"bigint\",\"literalArguments\":[],\"typeArguments\":[]}]}");
+        assertEquals(legacy, signature);
     }
 
     private static void assertJsonRoundTrip(ClientTypeSignature signature)