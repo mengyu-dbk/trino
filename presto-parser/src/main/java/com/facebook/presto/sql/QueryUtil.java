mengyu-dbk/trino

commit 3b66b83734c8e734cf74158b18760a7e65f0b746

Add singleValueQuery method to QueryUtil

presto-parser/src/main/java/com/facebook/presto/sql/QueryUtil.java
@@ -181,6 +181,19 @@ public final class QueryUtil
                 limit));
     }
 
+    public static Query singleValueQuery(String columnName, String value)
+    {
+        return query(new QuerySpecification(
+                new Select(false, ImmutableList.of(
+                        new SingleColumn(new StringLiteral(value), Optional.of(columnName)))),
+                Optional.empty(),
+                Optional.empty(),
+                Optional.empty(),
+                Optional.empty(),
+                ImmutableList.of(),
+                Optional.empty()));
+    }
+
     public static Query query(QueryBody body)
     {
         return new Query(

presto-main/src/main/java/com/facebook/presto/sql/analyzer/StatementAnalyzer.java
@@ -154,10 +154,10 @@ import static com.facebook.presto.sql.QueryUtil.row;
 import static com.facebook.presto.sql.QueryUtil.selectAll;
 import static com.facebook.presto.sql.QueryUtil.selectList;
 import static com.facebook.presto.sql.QueryUtil.simpleQuery;
+import static com.facebook.presto.sql.QueryUtil.singleValueQuery;
 import static com.facebook.presto.sql.QueryUtil.subquery;
 import static com.facebook.presto.sql.QueryUtil.table;
 import static com.facebook.presto.sql.QueryUtil.unaliasedName;
-import static com.facebook.presto.sql.QueryUtil.values;
 import static com.facebook.presto.sql.analyzer.ExpressionAnalyzer.createConstantAnalyzer;
 import static com.facebook.presto.sql.analyzer.ExpressionAnalyzer.getExpressionTypes;
 import static com.facebook.presto.sql.analyzer.SemanticErrorCode.AMBIGUOUS_ATTRIBUTE;
@@ -777,16 +777,9 @@ class StatementAnalyzer
             }
         }
 
-        String queryPlan = getQueryPlan(node, planType, planFormat);
+        String plan = getQueryPlan(node, planType, planFormat);
 
-        Query query = simpleQuery(
-                selectList(new AllColumns()),
-                aliased(
-                        values(row(new StringLiteral((queryPlan)))),
-                        "plan",
-                        ImmutableList.of("Query Plan")));
-
-        return process(query, context);
+        return process(singleValueQuery("Query Plan", plan), context);
     }
 
     private String getQueryPlan(Explain node, ExplainType.Type planType, ExplainFormat.Type planFormat)

commit f5a9e25950375624ea92d4ca50b4e3a0d5d5e1d5

Fix EXPLAIN tests
The tests were broken by 3b66b83734c8e734cf74158b18760a7e65f0b746
due to an additional exchange added for local parallelism
for a projection over a VALUES node.

presto-parser/src/main/java/com/facebook/presto/sql/QueryUtil.java
@@ -14,6 +14,7 @@
 package com.facebook.presto.sql;
 
 import com.facebook.presto.sql.tree.AliasedRelation;
+import com.facebook.presto.sql.tree.AllColumns;
 import com.facebook.presto.sql.tree.CoalesceExpression;
 import com.facebook.presto.sql.tree.ComparisonExpression;
 import com.facebook.presto.sql.tree.Expression;
@@ -183,15 +184,10 @@ public final class QueryUtil
 
     public static Query singleValueQuery(String columnName, String value)
     {
-        return query(new QuerySpecification(
-                new Select(false, ImmutableList.of(
-                        new SingleColumn(new StringLiteral(value), Optional.of(columnName)))),
-                Optional.empty(),
-                Optional.empty(),
-                Optional.empty(),
-                Optional.empty(),
-                ImmutableList.of(),
-                Optional.empty()));
+        Relation values = values(row(new StringLiteral((value))));
+        return simpleQuery(
+                selectList(new AllColumns()),
+                aliased(values, "t", ImmutableList.of(columnName)));
     }
 
     public static Query query(QueryBody body)