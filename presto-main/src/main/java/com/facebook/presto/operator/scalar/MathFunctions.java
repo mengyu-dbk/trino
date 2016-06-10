mengyu-dbk/trino

commit 8b3ae27d7dd28fd05b048352be1fc10e4218e428

Make round use native Math.round instead of Math.floor
This solves problem with rounding value of 0x1.fffffffffffffp-2
and makes our rounding strategy consistent with Java's standard library (round towards positive infinity).

presto-main/src/main/java/com/facebook/presto/operator/scalar/MathFunctions.java
@@ -465,15 +465,8 @@ public final class MathFunctions
     @SqlType(StandardTypes.DOUBLE)
     public static double round(@SqlType(StandardTypes.DOUBLE) double num, @SqlType(StandardTypes.BIGINT) long decimals)
     {
-        if (num == 0.0) {
-            return 0;
-        }
-        if (num < 0) {
-            return -round(-num, decimals);
-        }
-
         double factor = Math.pow(10, decimals);
-        return Math.floor(num * factor + 0.5) / factor;
+        return Math.round(num * factor) / factor;
     }
 
     @Description("signum")

presto-main/src/test/java/com/facebook/presto/operator/scalar/TestMathFunctions.java
@@ -439,7 +439,7 @@ public class TestMathFunctions
         assertFunction("round( 3.499)", DOUBLE, 3.0);
         assertFunction("round(-3.499)", DOUBLE, -3.0);
         assertFunction("round( 3.5)", DOUBLE, 4.0);
-        assertFunction("round(-3.5)", DOUBLE, -4.0);
+        assertFunction("round(-3.5)", DOUBLE, -3.0);
         assertFunction("round(-3.5001)", DOUBLE, -4.0);
         assertFunction("round(-3.99)", DOUBLE, -4.0);
         assertFunction("round(CAST(NULL as DOUBLE))", DOUBLE, null);
@@ -458,7 +458,7 @@ public class TestMathFunctions
         assertFunction("round( 3.499, 0)", DOUBLE, 3.0);
         assertFunction("round(-3.499, 0)", DOUBLE, -3.0);
         assertFunction("round( 3.5, 0)", DOUBLE, 4.0);
-        assertFunction("round(-3.5, 0)", DOUBLE, -4.0);
+        assertFunction("round(-3.5, 0)", DOUBLE, -3.0);
         assertFunction("round(-3.5001, 0)", DOUBLE, -4.0);
         assertFunction("round(-3.99, 0)", DOUBLE, -4.0);
         assertFunction("round(" + GREATEST_DOUBLE_LESS_THAN_HALF + ", 0)", DOUBLE, 0.0);

commit 91bc06e83396512f86a3538221114327ab2cc620

Make round() return NaN when input is NaN
This was a regression in a recent change: 8b3ae27d

presto-main/src/main/java/com/facebook/presto/operator/scalar/MathFunctions.java
@@ -465,6 +465,10 @@ public final class MathFunctions
     @SqlType(StandardTypes.DOUBLE)
     public static double round(@SqlType(StandardTypes.DOUBLE) double num, @SqlType(StandardTypes.BIGINT) long decimals)
     {
+        if (Double.isNaN(num)) {
+            return num;
+        }
+
         double factor = Math.pow(10, decimals);
         return Math.round(num * factor) / factor;
     }

presto-main/src/test/java/com/facebook/presto/operator/scalar/TestMathFunctions.java
@@ -485,6 +485,8 @@ public class TestMathFunctions
         assertFunction("round(CAST(NULL as DOUBLE), CAST(NULL as BIGINT))", DOUBLE, null);
         assertFunction("round(-3.0, CAST(NULL as BIGINT))", DOUBLE, null);
         assertFunction("round(CAST(NULL as DOUBLE), 1)", DOUBLE, null);
+
+        assertFunction("round(nan(), 2)", DOUBLE, Double.NaN);
     }
 
     @Test