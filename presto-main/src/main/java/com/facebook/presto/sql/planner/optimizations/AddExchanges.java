mengyu-dbk/trino

commit 9556186d3a8d231792675328103e3529c9d21f2e

Optimize sort node whose input is already sorted

presto-main/src/main/java/com/facebook/presto/sql/planner/optimizations/AddExchanges.java
@@ -474,6 +474,20 @@ public class AddExchanges
                         gatheringExchange(idAllocator.getNextId(), REMOTE, child.getNode()),
                         child.getProperties());
             }
+            else {
+                // current plan so far is single node, so local properties are effectively global properties
+                // skip the SortNode if the local properties guarantee ordering on Sort keys
+                // TODO: This should be extracted as a separate optimizer once the planner is able to reason about the ordering of each operator
+                List<LocalProperty<Symbol>> desiredProperties = new ArrayList<>();
+                for (Symbol symbol : node.getOrderBy()) {
+                    desiredProperties.add(new SortingProperty<>(symbol, node.getOrderings().get(symbol)));
+                }
+
+                if (LocalProperties.match(child.getProperties().getLocalProperties(), desiredProperties).stream()
+                        .noneMatch(Optional::isPresent)) {
+                    return child;
+                }
+            }
 
             return rebaseAndDeriveProperties(node, child);
         }

presto-main/src/test/java/com/facebook/presto/sql/planner/assertions/Matcher.java
@@ -25,7 +25,7 @@ public interface Matcher
      * should be limited to tests that validate the type of the node or
      * attributes of that type.
      * <p>
-     * Matchers that can be applied to nodes of any typeshould return true from
+     * Matchers that can be applied to nodes of any type should return true from
      * shapeMatches and do the rest of their work in detailMatches.
      *
      * @param node The node to apply the matching tests to

presto-main/src/test/java/com/facebook/presto/sql/planner/assertions/PlanMatchPattern.java
@@ -32,6 +32,7 @@ import com.facebook.presto.sql.planner.plan.OutputNode;
 import com.facebook.presto.sql.planner.plan.PlanNode;
 import com.facebook.presto.sql.planner.plan.ProjectNode;
 import com.facebook.presto.sql.planner.plan.SemiJoinNode;
+import com.facebook.presto.sql.planner.plan.SortNode;
 import com.facebook.presto.sql.planner.plan.TableScanNode;
 import com.facebook.presto.sql.planner.plan.UnionNode;
 import com.facebook.presto.sql.planner.plan.ValuesNode;
@@ -176,6 +177,11 @@ public final class PlanMatchPattern
         assignments.entrySet().forEach(
                 assignment -> result.withAlias(assignment.getKey(), new WindowFunctionMatcher(assignment.getValue())));
         return result;
+    }
+
+    public static PlanMatchPattern sort(PlanMatchPattern source)
+    {
+        return node(SortNode.class, source);
     }
 
     public static PlanMatchPattern output(PlanMatchPattern source)

presto-main/src/test/java/com/facebook/presto/sql/planner/optimizations/TestEliminateSorts.java
@@ -1,0 +1,101 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.sql.planner.optimizations;
+
+import com.facebook.presto.spi.block.SortOrder;
+import com.facebook.presto.sql.parser.SqlParser;
+import com.facebook.presto.sql.planner.Plan;
+import com.facebook.presto.sql.planner.assertions.BasePlanTest;
+import com.facebook.presto.sql.planner.assertions.ExpectedValueProvider;
+import com.facebook.presto.sql.planner.assertions.PlanAssert;
+import com.facebook.presto.sql.planner.assertions.PlanMatchPattern;
+import com.facebook.presto.sql.planner.plan.WindowNode;
+import com.facebook.presto.testing.LocalQueryRunner;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import org.intellij.lang.annotations.Language;
+import org.testng.annotations.Test;
+
+import java.util.List;
+import java.util.Optional;
+
+import static com.facebook.presto.sql.planner.assertions.PlanMatchPattern.anyTree;
+import static com.facebook.presto.sql.planner.assertions.PlanMatchPattern.functionCall;
+import static com.facebook.presto.sql.planner.assertions.PlanMatchPattern.output;
+import static com.facebook.presto.sql.planner.assertions.PlanMatchPattern.project;
+import static com.facebook.presto.sql.planner.assertions.PlanMatchPattern.sort;
+import static com.facebook.presto.sql.planner.assertions.PlanMatchPattern.specification;
+import static com.facebook.presto.sql.planner.assertions.PlanMatchPattern.tableScan;
+import static com.facebook.presto.sql.planner.assertions.PlanMatchPattern.window;
+
+public class TestEliminateSorts
+        extends BasePlanTest
+{
+    private static final String QUANTITY_ALIAS = "QUANTITY";
+
+    private static final ExpectedValueProvider<WindowNode.Specification> windowSpec = specification(
+            ImmutableList.of(),
+            ImmutableList.of(QUANTITY_ALIAS),
+            ImmutableMap.of(QUANTITY_ALIAS, SortOrder.ASC_NULLS_LAST));
+
+    private static final PlanMatchPattern LINEITEM_TABLESCAN_Q = tableScan(
+            "lineitem",
+            ImmutableMap.of(QUANTITY_ALIAS, "quantity"));
+
+    @Test
+    public void testEliminateSorts()
+    {
+        @Language("SQL") String sql = "SELECT quantity, row_number() OVER (ORDER BY quantity) FROM lineitem ORDER BY quantity";
+
+        PlanMatchPattern pattern =
+                output(
+                        window(windowSpec,
+                                ImmutableList.of(functionCall("row_number", Optional.empty(), ImmutableList.of())),
+                                anyTree(LINEITEM_TABLESCAN_Q)));
+
+        assertUnitPlan(sql, pattern);
+    }
+
+    @Test
+    public void testNotEliminateSorts()
+    {
+        @Language("SQL") String sql = "SELECT quantity, row_number() OVER (ORDER BY quantity) FROM lineitem ORDER BY tax";
+
+        PlanMatchPattern pattern =
+                anyTree(
+                        sort(
+                                project(window(windowSpec,
+                                        ImmutableList.of(functionCall("row_number", Optional.empty(), ImmutableList.of())),
+                                        anyTree(LINEITEM_TABLESCAN_Q)))));
+
+        assertUnitPlan(sql, pattern);
+    }
+
+    public void assertUnitPlan(@Language("SQL") String sql, PlanMatchPattern pattern)
+    {
+        LocalQueryRunner queryRunner = getQueryRunner();
+        List<PlanOptimizer> optimizers = ImmutableList.of(
+                new UnaliasSymbolReferences(),
+                new AddExchanges(queryRunner.getMetadata(), new SqlParser()),
+                new PruneUnreferencedOutputs(),
+                new PruneIdentityProjections()
+        );
+
+        queryRunner.inTransaction(transactionSession -> {
+            Plan actualPlan = queryRunner.createPlan(transactionSession, sql, optimizers);
+            PlanAssert.assertPlan(transactionSession, queryRunner.getMetadata(), actualPlan, pattern);
+            return null;
+        });
+    }
+}

commit d9c59a217e4ffa344ed14cadf8bccf3296b68619

Fix bad merge in AddExchanges
Changes in commit 9556186d3a8d231792675328103e3529c9d21f2e mistakenly
got merged into visitLimit instead of visitSort.

presto-main/src/main/java/com/facebook/presto/sql/planner/optimizations/AddExchanges.java
@@ -456,6 +456,20 @@ public class AddExchanges
                         gatheringExchange(idAllocator.getNextId(), REMOTE, child.getNode()),
                         child.getProperties());
             }
+            else {
+                // current plan so far is single node, so local properties are effectively global properties
+                // skip the SortNode if the local properties guarantee ordering on Sort keys
+                // TODO: This should be extracted as a separate optimizer once the planner is able to reason about the ordering of each operator
+                List<LocalProperty<Symbol>> desiredProperties = new ArrayList<>();
+                for (Symbol symbol : node.getOrderBy()) {
+                    desiredProperties.add(new SortingProperty<>(symbol, node.getOrderings().get(symbol)));
+                }
+
+                if (LocalProperties.match(child.getProperties().getLocalProperties(), desiredProperties).stream()
+                        .noneMatch(Optional::isPresent)) {
+                    return child;
+                }
+            }
 
             return rebaseAndDeriveProperties(node, child);
         }
@@ -473,20 +487,6 @@ public class AddExchanges
                 child = withDerivedProperties(
                         gatheringExchange(idAllocator.getNextId(), REMOTE, child.getNode()),
                         child.getProperties());
-            }
-            else {
-                // current plan so far is single node, so local properties are effectively global properties
-                // skip the SortNode if the local properties guarantee ordering on Sort keys
-                // TODO: This should be extracted as a separate optimizer once the planner is able to reason about the ordering of each operator
-                List<LocalProperty<Symbol>> desiredProperties = new ArrayList<>();
-                for (Symbol symbol : node.getOrderBy()) {
-                    desiredProperties.add(new SortingProperty<>(symbol, node.getOrderings().get(symbol)));
-                }
-
-                if (LocalProperties.match(child.getProperties().getLocalProperties(), desiredProperties).stream()
-                        .noneMatch(Optional::isPresent)) {
-                    return child;
-                }
             }
 
             return rebaseAndDeriveProperties(node, child);