mengyu-dbk/trino

commit 42eaf9236b8f5a5ceca6b34558e6fa05dfbbdc34

Minor refactor in HivePartitionManager and HiveSplitManager
* Remove unused PRESTO_OFFLINE constant in HivePartitionManager,
  as table/partition online verification has been refactored into
  MetastoreUtil.verifyOnline.

* Refactor to use MetastoreUtil.verifyOnline in HiveSplitManager.

presto-hive/src/main/java/com/facebook/presto/hive/HiveSplitManager.java
@@ -35,7 +35,6 @@ import com.google.common.collect.Ordering;
 import io.airlift.concurrent.BoundedExecutor;
 import io.airlift.stats.CounterStat;
 import io.airlift.units.DataSize;
-import org.apache.hadoop.hive.metastore.ProtectMode;
 import org.weakref.jmx.Managed;
 import org.weakref.jmx.Nested;
 
@@ -54,12 +53,13 @@ import static com.facebook.presto.hive.HiveErrorCode.HIVE_INVALID_METADATA;
 import static com.facebook.presto.hive.HiveErrorCode.HIVE_PARTITION_DROPPED_DURING_QUERY;
 import static com.facebook.presto.hive.HiveErrorCode.HIVE_PARTITION_SCHEMA_MISMATCH;
 import static com.facebook.presto.hive.HivePartition.UNPARTITIONED_ID;
+import static com.facebook.presto.hive.metastore.MetastoreUtil.getProtectMode;
 import static com.facebook.presto.hive.metastore.MetastoreUtil.makePartName;
+import static com.facebook.presto.hive.metastore.MetastoreUtil.verifyOnline;
 import static com.facebook.presto.spi.StandardErrorCode.GENERIC_INTERNAL_ERROR;
 import static com.facebook.presto.spi.StandardErrorCode.SERVER_SHUTTING_DOWN;
 import static com.facebook.presto.spi.connector.ConnectorSplitManager.SplitSchedulingStrategy.GROUPED_SCHEDULING;
 import static com.google.common.base.Preconditions.checkArgument;
-import static com.google.common.base.Strings.isNullOrEmpty;
 import static com.google.common.collect.Iterables.concat;
 import static com.google.common.collect.Iterables.getOnlyElement;
 import static com.google.common.collect.Iterables.transform;
@@ -67,7 +67,6 @@ import static io.airlift.units.DataSize.Unit.MEGABYTE;
 import static java.lang.Math.min;
 import static java.lang.String.format;
 import static java.util.Objects.requireNonNull;
-import static org.apache.hadoop.hive.metastore.ProtectMode.getProtectModeFromString;
 
 public class HiveSplitManager
         implements ConnectorSplitManager
@@ -269,17 +268,10 @@ public class HiveSplitManager
                 if (partition == null) {
                     throw new PrestoException(GENERIC_INTERNAL_ERROR, "Partition not loaded: " + hivePartition);
                 }
-
-                // verify all partition is online
-                String protectMode = partition.getParameters().get(ProtectMode.PARAMETER_NAME);
                 String partName = makePartName(table.getPartitionColumns(), partition.getValues());
-                if (protectMode != null && getProtectModeFromString(protectMode).offline) {
-                    throw new PartitionOfflineException(tableName, partName, false, null);
-                }
-                String prestoOffline = partition.getParameters().get(PRESTO_OFFLINE);
-                if (!isNullOrEmpty(prestoOffline)) {
-                    throw new PartitionOfflineException(tableName, partName, true, prestoOffline);
-                }
+
+                // verify partition is online
+                verifyOnline(tableName, Optional.of(partName), getProtectMode(partition), table.getParameters());
 
                 // Verify that the partition schema matches the table schema.
                 // Either adding or dropping columns from the end of the table

presto-hive/src/main/java/com/facebook/presto/hive/HivePartitionManager.java
@@ -76,7 +76,6 @@ import static java.util.stream.Collectors.toList;
 
 public class HivePartitionManager
 {
-    public static final String PRESTO_OFFLINE = "presto_offline";
     private static final String PARTITION_VALUE_WILDCARD = "";
 
     private final DateTimeZone timeZone;

commit d04a0d2fbee11fee85005deaaf23d73ad7f0b99b

Fix partition offline check in HiveSplitManager
It is checking the table parameters rather than partition parameters.
The bug is introduced in 42eaf9236b8f5a5ceca6b34558e6fa05dfbbdc34.

presto-hive/src/main/java/com/facebook/presto/hive/HiveSplitManager.java
@@ -288,7 +288,7 @@ public class HiveSplitManager
                 String partName = makePartName(table.getPartitionColumns(), partition.getValues());
 
                 // verify partition is online
-                verifyOnline(tableName, Optional.of(partName), getProtectMode(partition), table.getParameters());
+                verifyOnline(tableName, Optional.of(partName), getProtectMode(partition), partition.getParameters());
 
                 // verify partition is not marked as non-readable
                 String partitionNotReadable = partition.getParameters().get(OBJECT_NOT_READABLE);