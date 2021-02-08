mengyu-dbk/trino

commit 4b9dc7cd67088beb94beb4d83fdbc4c5686523b0

Add generated name sequencer to JdbcTableHandle
During query pushdown we need to create synthetic columns, with unique
names. Inspecting existing columns is only as useful, as an subquery
enclosed in a table handle can some names used but not visible.

plugin/trino-base-jdbc/src/main/java/io/trino/plugin/jdbc/JdbcMetadata.java
@@ -153,7 +153,8 @@ public class JdbcMetadata
                 handle.getRelationHandle(),
                 newDomain,
                 handle.getLimit(),
-                handle.getColumns());
+                handle.getColumns(),
+                handle.getMextSyntheticColumnId());
 
         return Optional.of(new ConstraintApplicationResult<>(handle, remainingFilter));
     }
@@ -166,7 +167,8 @@ public class JdbcMetadata
                 new JdbcQueryRelationHandle(preparedQuery),
                 TupleDomain.all(),
                 OptionalLong.empty(),
-                Optional.of(columns));
+                Optional.of(columns),
+                handle.getMextSyntheticColumnId());
     }
 
     @Override
@@ -191,7 +193,8 @@ public class JdbcMetadata
                         handle.getRelationHandle(),
                         handle.getConstraint(),
                         handle.getLimit(),
-                        Optional.of(newColumns)),
+                        Optional.of(newColumns),
+                        handle.getMextSyntheticColumnId()),
                 projections,
                 assignments.entrySet().stream()
                         .map(assignment -> new Assignment(
@@ -227,11 +230,7 @@ public class JdbcMetadata
             handle = flushAttributesAsQuery(session, handle);
         }
 
-        List<JdbcColumnHandle> columns = jdbcClient.getColumns(session, handle);
-        Map<String, JdbcColumnHandle> columnByName = columns.stream()
-                .collect(toImmutableMap(JdbcColumnHandle::getColumnName, identity()));
-
-        int syntheticNextIdentifier = 1;
+        int mextSyntheticColumnId = handle.getMextSyntheticColumnId();
 
         ImmutableList.Builder<JdbcColumnHandle> newColumns = ImmutableList.builder();
         ImmutableList.Builder<ConnectorExpression> projections = ImmutableList.builder();
@@ -243,11 +242,8 @@ public class JdbcMetadata
                 return Optional.empty();
             }
 
-            while (columnByName.containsKey(SYNTHETIC_COLUMN_NAME_PREFIX + syntheticNextIdentifier)) {
-                syntheticNextIdentifier++;
-            }
-
-            String columnName = SYNTHETIC_COLUMN_NAME_PREFIX + syntheticNextIdentifier;
+            String columnName = SYNTHETIC_COLUMN_NAME_PREFIX + mextSyntheticColumnId;
+            mextSyntheticColumnId++;
             JdbcColumnHandle newColumn = JdbcColumnHandle.builder()
                     .setColumnName(columnName)
                     .setJdbcTypeHandle(expression.get().getJdbcTypeHandle())
@@ -259,8 +255,6 @@ public class JdbcMetadata
             projections.add(new Variable(newColumn.getColumnName(), aggregate.getOutputType()));
             resultAssignments.add(new Assignment(newColumn.getColumnName(), newColumn, aggregate.getOutputType()));
             expressions.put(columnName, expression.get().getExpression());
-
-            syntheticNextIdentifier++;
         }
 
         List<List<JdbcColumnHandle>> groupingSetsAsJdbcColumnHandles = groupingSets.stream()
@@ -287,7 +281,8 @@ public class JdbcMetadata
                 new JdbcQueryRelationHandle(preparedQuery),
                 TupleDomain.all(),
                 OptionalLong.empty(),
-                Optional.of(newColumnsList));
+                Optional.of(newColumnsList),
+                mextSyntheticColumnId);
 
         return Optional.of(new AggregationApplicationResult<>(handle, projections.build(), resultAssignments.build(), ImmutableMap.of()));
     }
@@ -309,7 +304,8 @@ public class JdbcMetadata
                 handle.getRelationHandle(),
                 handle.getConstraint(),
                 OptionalLong.of(limit),
-                handle.getColumns());
+                handle.getColumns(),
+                handle.getMextSyntheticColumnId());
 
         return Optional.of(new LimitApplicationResult<>(handle, jdbcClient.isLimitGuaranteed(session)));
     }

plugin/trino-base-jdbc/src/main/java/io/trino/plugin/jdbc/JdbcTableHandle.java
@@ -45,6 +45,8 @@ public final class JdbcTableHandle
     // columns of the relation described by this handle
     private final Optional<List<JdbcColumnHandle>> columns;
 
+    private final int nextSyntheticColumnId;
+
     @Deprecated
     public JdbcTableHandle(SchemaTableName schemaTableName, @Nullable String catalogName, @Nullable String schemaName, String tableName)
     {
@@ -57,7 +59,8 @@ public final class JdbcTableHandle
                 new JdbcNamedRelationHandle(schemaTableName, remoteTableName),
                 TupleDomain.all(),
                 OptionalLong.empty(),
-                Optional.empty());
+                Optional.empty(),
+                0);
     }
 
     @JsonCreator
@@ -65,7 +68,8 @@ public final class JdbcTableHandle
             @JsonProperty("relationHandle") JdbcRelationHandle relationHandle,
             @JsonProperty("constraint") TupleDomain<ColumnHandle> constraint,
             @JsonProperty("limit") OptionalLong limit,
-            @JsonProperty("columns") Optional<List<JdbcColumnHandle>> columns)
+            @JsonProperty("columns") Optional<List<JdbcColumnHandle>> columns,
+            @JsonProperty("nextSyntheticColumnId") int nextSyntheticColumnId)
     {
         this.relationHandle = requireNonNull(relationHandle, "relationHandle is null");
         this.constraint = requireNonNull(constraint, "constraint is null");
@@ -74,6 +78,7 @@ public final class JdbcTableHandle
 
         requireNonNull(columns, "columns is null");
         this.columns = columns.map(ImmutableList::copyOf);
+        this.nextSyntheticColumnId = nextSyntheticColumnId;
     }
 
     /**
@@ -170,6 +175,12 @@ public final class JdbcTableHandle
         return columns;
     }
 
+    @JsonProperty
+    public int getMextSyntheticColumnId()
+    {
+        return nextSyntheticColumnId;
+    }
+
     @JsonIgnore
     public boolean isSynthetic()
     {
@@ -195,13 +206,14 @@ public final class JdbcTableHandle
         return Objects.equals(this.relationHandle, o.relationHandle) &&
                 Objects.equals(this.constraint, o.constraint) &&
                 Objects.equals(this.limit, o.limit) &&
-                Objects.equals(this.columns, o.columns);
+                Objects.equals(this.columns, o.columns) &&
+                this.nextSyntheticColumnId == o.nextSyntheticColumnId;
     }
 
     @Override
     public int hashCode()
     {
-        return Objects.hash(relationHandle, constraint, limit, columns);
+        return Objects.hash(relationHandle, constraint, limit, columns, nextSyntheticColumnId);
     }
 
     @Override

plugin/trino-druid/src/main/java/io/trino/plugin/druid/DruidJdbcClient.java
@@ -187,7 +187,8 @@ public class DruidJdbcClient
                                     table.getRequiredNamedRelation().getRemoteTableName().getTableName())),
                     table.getConstraint(),
                     table.getLimit(),
-                    table.getColumns());
+                    table.getColumns(),
+                    table.getMextSyntheticColumnId());
         }
 
         return table;

plugin/trino-base-jdbc/src/test/java/io/trino/plugin/jdbc/TestJdbcRecordSetProvider.java
@@ -187,7 +187,8 @@ public class TestJdbcRecordSetProvider
                 jdbcTableHandle.getRelationHandle(),
                 domain,
                 OptionalLong.empty(),
-                Optional.empty());
+                Optional.empty(),
+                jdbcTableHandle.getMextSyntheticColumnId());
 
         ConnectorSplitSource splits = jdbcClient.getSplits(SESSION, jdbcTableHandle);
         JdbcSplit split = (JdbcSplit) getOnlyElement(getFutureValue(splits.getNextBatch(NOT_PARTITIONED, 1000)).getSplits());

plugin/trino-base-jdbc/src/test/java/io/trino/plugin/jdbc/TestJdbcMetadata.java
@@ -312,7 +312,7 @@ public class TestJdbcMetadata
                 Optional.of(ImmutableMap.of(groupByColumn, secondDomain)));
         assertEquals(
                 ((JdbcQueryRelationHandle) tableHandleWithFilter.getRelationHandle()).getPreparedQuery().getQuery(),
-                "SELECT \"TEXT\", count(*) AS \"_pfgnrtd_1\" " +
+                "SELECT \"TEXT\", count(*) AS \"_pfgnrtd_0\" " +
                         "FROM \"" + database.getDatabaseName() + "\".\"EXAMPLE\".\"NUMBERS\" " +
                         "WHERE \"TEXT\" IN (?,?) " +
                         "GROUP BY \"TEXT\"");
@@ -341,7 +341,7 @@ public class TestJdbcMetadata
                 Optional.of(ImmutableMap.of(nonGroupByColumn, domain)));
         assertEquals(
                 ((JdbcQueryRelationHandle) tableHandleWithFilter.getRelationHandle()).getPreparedQuery().getQuery(),
-                "SELECT \"TEXT\", count(*) AS \"_pfgnrtd_1\" " +
+                "SELECT \"TEXT\", count(*) AS \"_pfgnrtd_0\" " +
                         "FROM \"" + database.getDatabaseName() + "\".\"EXAMPLE\".\"NUMBERS\" " +
                         "GROUP BY \"TEXT\"");
     }
@@ -370,7 +370,7 @@ public class TestJdbcMetadata
                 Optional.of(ImmutableMap.of(valueColumn, domain)));
         assertEquals(
                 ((JdbcQueryRelationHandle) tableHandleWithFilter.getRelationHandle()).getPreparedQuery().getQuery(),
-                "SELECT \"TEXT\", \"VALUE\", count(*) AS \"_pfgnrtd_1\" " +
+                "SELECT \"TEXT\", \"VALUE\", count(*) AS \"_pfgnrtd_0\" " +
                         "FROM \"" + database.getDatabaseName() + "\".\"EXAMPLE\".\"NUMBERS\" " +
                         "GROUP BY GROUPING SETS ((\"TEXT\", \"VALUE\"), (\"TEXT\"))");
     }

commit 3da7be4cf10babbe2f91c5c153804284e6cdf229

Fix typo in property name
It was recently introduced in 4b9dc7cd67088beb94beb4d83fdbc4c5686523b0.

plugin/trino-base-jdbc/src/main/java/io/trino/plugin/jdbc/JdbcMetadata.java
@@ -154,7 +154,7 @@ public class JdbcMetadata
                 newDomain,
                 handle.getLimit(),
                 handle.getColumns(),
-                handle.getMextSyntheticColumnId());
+                handle.getNextSyntheticColumnId());
 
         return Optional.of(new ConstraintApplicationResult<>(handle, remainingFilter));
     }
@@ -168,7 +168,7 @@ public class JdbcMetadata
                 TupleDomain.all(),
                 OptionalLong.empty(),
                 Optional.of(columns),
-                handle.getMextSyntheticColumnId());
+                handle.getNextSyntheticColumnId());
     }
 
     @Override
@@ -194,7 +194,7 @@ public class JdbcMetadata
                         handle.getConstraint(),
                         handle.getLimit(),
                         Optional.of(newColumns),
-                        handle.getMextSyntheticColumnId()),
+                        handle.getNextSyntheticColumnId()),
                 projections,
                 assignments.entrySet().stream()
                         .map(assignment -> new Assignment(
@@ -230,7 +230,7 @@ public class JdbcMetadata
             handle = flushAttributesAsQuery(session, handle);
         }
 
-        int mextSyntheticColumnId = handle.getMextSyntheticColumnId();
+        int nextSyntheticColumnId = handle.getNextSyntheticColumnId();
 
         ImmutableList.Builder<JdbcColumnHandle> newColumns = ImmutableList.builder();
         ImmutableList.Builder<ConnectorExpression> projections = ImmutableList.builder();
@@ -242,8 +242,8 @@ public class JdbcMetadata
                 return Optional.empty();
             }
 
-            String columnName = SYNTHETIC_COLUMN_NAME_PREFIX + mextSyntheticColumnId;
-            mextSyntheticColumnId++;
+            String columnName = SYNTHETIC_COLUMN_NAME_PREFIX + nextSyntheticColumnId;
+            nextSyntheticColumnId++;
             JdbcColumnHandle newColumn = JdbcColumnHandle.builder()
                     .setColumnName(columnName)
                     .setJdbcTypeHandle(expression.get().getJdbcTypeHandle())
@@ -282,7 +282,7 @@ public class JdbcMetadata
                 TupleDomain.all(),
                 OptionalLong.empty(),
                 Optional.of(newColumnsList),
-                mextSyntheticColumnId);
+                nextSyntheticColumnId);
 
         return Optional.of(new AggregationApplicationResult<>(handle, projections.build(), resultAssignments.build(), ImmutableMap.of()));
     }
@@ -305,7 +305,7 @@ public class JdbcMetadata
                 handle.getConstraint(),
                 OptionalLong.of(limit),
                 handle.getColumns(),
-                handle.getMextSyntheticColumnId());
+                handle.getNextSyntheticColumnId());
 
         return Optional.of(new LimitApplicationResult<>(handle, jdbcClient.isLimitGuaranteed(session)));
     }

plugin/trino-base-jdbc/src/main/java/io/trino/plugin/jdbc/JdbcTableHandle.java
@@ -176,7 +176,7 @@ public final class JdbcTableHandle
     }
 
     @JsonProperty
-    public int getMextSyntheticColumnId()
+    public int getNextSyntheticColumnId()
     {
         return nextSyntheticColumnId;
     }

plugin/trino-druid/src/main/java/io/trino/plugin/druid/DruidJdbcClient.java
@@ -188,7 +188,7 @@ public class DruidJdbcClient
                     table.getConstraint(),
                     table.getLimit(),
                     table.getColumns(),
-                    table.getMextSyntheticColumnId());
+                    table.getNextSyntheticColumnId());
         }
 
         return table;

plugin/trino-base-jdbc/src/test/java/io/trino/plugin/jdbc/TestJdbcRecordSetProvider.java
@@ -188,7 +188,7 @@ public class TestJdbcRecordSetProvider
                 domain,
                 OptionalLong.empty(),
                 Optional.empty(),
-                jdbcTableHandle.getMextSyntheticColumnId());
+                jdbcTableHandle.getNextSyntheticColumnId());
 
         ConnectorSplitSource splits = jdbcClient.getSplits(SESSION, jdbcTableHandle);
         JdbcSplit split = (JdbcSplit) getOnlyElement(getFutureValue(splits.getNextBatch(NOT_PARTITIONED, 1000)).getSplits());